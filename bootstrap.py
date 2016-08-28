7e174f3991213d797725a04bac7218e86eebc9c6
Christopher Jenness <christopherjenness@gmail.com>
2016-08-28 07:13:54 -0400

    Add parametric functionality boilerplate

--- bootstrap.py
+++ bootstrap.py
@@ -11,46 +11,65 @@ from scipy.stats import sem
 
 class Bootstrap:
     
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
         return
     
     
-    def resample(self, data):
+    def resample(self, data, parametric=False):
         """
         Resamples data by random sample with replacement
         
         Args:
             data (np.array): array of data to resample
+            parametric (str) in ['gaussian', 'uniform']: parametric distribution to resample from
+                if False, use nonparametric bootstrap
         
         Returns:
-            array: array of resampled data
+            resampled data (array)
         """
-        sample_size = len(data)
-        return [np.random.randint(0, sample_size) for i in range(sample_size)]
-
-    def estimate_standard_error(self, data, func=np.mean, n_samples=50):
+        if parametric == 'gaussian':
+            # TODO code for gaussian resampling
+            pass
+        elif parametric == 'uniform':
+            # TODO code for uniform resampling
+            pass
+        else:
+            sample_size = len(data)
+            indices = [np.random.randint(0, sample_size) for i in range(sample_size)]
+            return data[indices]
+        
+    def bootstrap_statistic(self, data, func=np.mean, n_samples=50, parametric=False):
         """
+        Bootstraps a statistic and calculates the standard error of the statistic
+        
         Args:
             data (array): array of data to calculate statistic and SE of statistic
             func (function): statistical function to calculate on data
                 examples: np.mean, np.median
             n_samples (int): number of bootstrap samples to 
+            
+        Returns:
+            statistic (tuple), sem (tuple)
+            Returns the bootstrapped statistic and the SEM of the statistic
         """
         statistics = []
         for sample in range(n_samples):
-            indices = self.resample(data)
-            statistic = func(data[indices])
+            resample = self.resample(data, parametric=parametric)
+            statistic = func(resample)
             statistics.append(statistic)
-            print (data[indices])
-        print (statistics)
-        return np.mean(statistics), sem(statistics)
+        return (np.mean(statistics), sem(statistics))
+        
 
-a = Bootstrap(np.random.uniform(0, 1, 100))
-a.resample(a.data)
-b = a.data[a.resample(a.data)]
-print(b)
-c = a.estimate_standard_error(a.data)
+data = np.random.uniform(0, 1, 100)
+bootstrap = Bootstrap()
+bootstrap_mean = bootstrap.bootstrap_statistic(data)
+print(bootstrap_mean)
+
+
+
+
+
+
 
 
 
