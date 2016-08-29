174907f67335b08b2ee5a4587769585a8015ca72
Christopher Jenness <christopherjenness@gmail.com>
2016-08-29 06:16:43 -0400

    Add initial teseting

---
+++ tests/test_resampling.py
@@ -0,0 +1,34 @@
+import unittest
+from bootstrap.bootstrap import Bootstrap
+import numpy as np
+
+class BootstrapInit(unittest.TestCase):
+    def setUp(self):
+        self.bootstrap = Bootstrap()
+        np.random.seed(0)
+        self.normal_data = np.random.normal(100, 10, size=100)
+        np.random.seed(0)
+        self.uniform_data = np.random.uniform(0, 100, size=100)
+        np.random.seed(0)
+        self.matrix_data = np.random.normal(100, 10, size=(100,100))
+    
+
+class ResamplingTestCase(BootstrapInit):
+    def testNonparametric(self):
+        bootstrap_data = self.bootstrap.bootstrap_sample(self.normal_data)
+        self.assertAlmostEqual(np.mean(self.normal_data)/10000, np.mean(bootstrap_data)/10000, 3)
+        self.assertEqual(len(bootstrap_data), len(self.normal_data))
+        
+    def testNormalParametric(self):
+        bootstrap_data = self.bootstrap.bootstrap_sample(self.normal_data, parametric='normal')
+        self.assertAlmostEqual(np.mean(self.normal_data)/10000, np.mean(bootstrap_data)/10000, 3)
+        self.assertEqual(len(bootstrap_data), len(self.normal_data))
+        
+    def testUniformParametric(self):
+        bootstrap_data = self.bootstrap.bootstrap_sample(self.uniform_data, parametric='uniform')
+        self.assertAlmostEqual(np.mean(self.uniform_data)/10000, np.mean(bootstrap_data)/10000, 3)
+        self.assertEqual(len(bootstrap_data), len(self.normal_data))
+
+if __name__ == '__main__':
+    unittest.main()
+    
